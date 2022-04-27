b66276818a200fdc6b17b44df82f820e08626552
JOÃO GABRIEL COSTA FLÔR <21276663@CFP127ALUNO.LOCAL>
2022-04-27 17:00:35 -0300

    CRUD.JS

---
+++ js/app.js
@@ -0,0 +1,79 @@
+'use strict'
+
+import { openModal, closeModal } from './modal.js'
+import { readClients, createClient, deleteClient } from './clients.js'
+
+const createRow = (client) => {
+    const row = document.createElement('tr')
+    row.innerHTML = `
+        <td>${client.nome}</td>
+        <td>${client.email}</td>
+        <td>${client.celular}</td>
+        <td>${client.cidade}</td>
+        <td>
+            <button type="button" class="button green" id="editar"-${client.id}>editar</button>
+            <button type="button" class="button red" id="excluir"-${client.id}>excluir</button>
+        </td>
+    `
+    return row
+}
+
+// Método para carregar os clientes quando carregar a página
+const uptadeTable = async () => {
+
+    const clientsContainer = document.getElementById('clients-container')
+
+    // Ler a API e armazenar o resultado em uma variavel
+    const clients = await readClients()
+
+    // Preencher a tabela com as informações
+    const rows = clients.map(createRow)
+
+    // Colocando elemento por elemento no id clientsContainer
+    clientsContainer.replaceChildren(...rows)
+}
+
+// Salvando umm Cliente quando um usuário cria um
+const saveClient = async() => {
+
+    // Criar um Json com as informações do cliente
+    const client = {
+        "id":       "",
+        "nome":     document.getElementById('nome').value,
+        "email":    document.getElementById('email').value,
+        "celular":  document.getElementById('celular').value,
+        "cidade":   document.getElementById('cidade').value
+    }
+    // Enviar o Json para o servidor API
+    await createClient(client)
+
+    // Fechar a Modal
+    closeModal()
+
+    // Atualizar a tabela
+    uptadeTable()
+}
+
+const actionClient = async (event) => {
+    
+    if(event.target.type == 'button'){
+
+        const [action, codigo] = event.target.id.split('-')
+
+        if (action == 'editar'){
+            // Funçõa para editar
+        } else if (action == 'excluir'){
+            await deleteClient(codigo)
+            
+            uptadeTable()
+        }
+    }
+}
+
+// Carregando a página chamando a função
+uptadeTable()
+
+// Eventos
+document.getElementById('cadastrarCliente').addEventListener('click', openModal)
+document.getElementById('salvar').addEventListener('click', saveClient)
+document.getElementById('clients-container').addEventListener('click', actionClient)
---
+++ js/clients.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// Consumindo a API do CLiente
+
+const url = 'https://testeleonid.herokuapp.com/clientes'
+
+const readClients = async() => {
+    const response = await fetch(url)
+    return await response.json()
+}
+
+const createClient = async(client) => {
+    const options = {
+        'method': 'POST',
+        'body': JSON.stringify(client),
+        'headers': {
+            'content-type': 'application/json'
+        }
+    }
+    const response = await fetch(url, options)
+    console.log (response.ok)
+}
+
+const deleteClient = async(codigo) => {
+    const options = {
+        'method': 'DELETE'
+    }
+    const response = await fetch(`${url}/${codigo}`, options)
+    console.log(response.ok)
+}
+
+export{readClients, createClient, deleteClient}
